6106a286db0b568571d625d9a7ea9dc72303cea2
Michael Regdosz <mregdos000@citymail.cuny.edu>
2025-02-20 21:18:55 -0500

    snappier

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+window.addEventListener('scroll', () => {
+    const nav = document.getElementById('nav');
+    const parallax = document.querySelector('.parallax');
+    let scrollPosition = window.pageYOffset;
+    parallax.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
+    if (scrollPosition > 100) {
+        nav.classList.add('sticky');
+    } else {
+        nav.classList.remove('sticky');
+    }
+});
